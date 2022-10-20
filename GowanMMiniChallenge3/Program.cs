0a06874131b17c93e862238e08db665f3801fc65
Madeline <116022163+MJGowan@users.noreply.github.com>
2022-10-20 15:02:26 -0700

    Add files via upload

--- GowanMMiniChallenge3/Program.cs
+++ GowanMMiniChallenge3/Program.cs
@@ -6,6 +6,10 @@
 //As a personal choice, I did not make it necessary to enter a number,
 //so the user may enter that they woke up at "6", "6:00", "six", "six o'clock", etc.
 
+//Peer review by : Caroline Hana -The code runs good I only ran into one issue which
+//can be such a simple fix. One of the if statements needs to be turned into a while
+//statement, because everytime i input something incorrect it just ends instead of
+//looping. Other then that the code ran and the other validations were correct.
 
 Console.Clear();
 Console.Write("Can I ask you some questions? Y or N: ");
